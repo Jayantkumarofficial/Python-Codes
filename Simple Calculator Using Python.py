b33d7f8a6d0de01072832fd741e56b76533cc436
Jayant Kumar <jayantkumarsofficial@gmail.com>
2025-05-16 15:54:12 +0530

    Add files via upload

---
+++ Simple Calculator Using Python.py
@@ -0,0 +1,20 @@
+a = int(input("enter the first number: "))
+b = int(input("Enter the second number: "))
+o = input("Enter the operator: ")
+
+
+
+if o == "+":
+    print(a+b)
+
+if o == "-":
+    print(a-b)
+
+if o == "*":
+    print(a*b)
+
+if o == "/":
+    print(a/b)
+
+
+
